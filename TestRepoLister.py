76143200044a01eebc5f29bd11f7d259256a07b5
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2021-10-02 21:38:39 -0400

    refined test cases

--- TestRepoLister.py
+++ TestRepoLister.py
@@ -6,20 +6,18 @@ class TestCommitCounter(unittest.TestCase):
 
     def test_HW2567(self):
         self.assertEqual(countCommits('Arceus1ooo', 'HW2-567'), 12)
-
     def test_HW1567(self):
         self.assertEqual(countCommits('Arceus1ooo', 'HW1-567'), 6)
-
     def test_helloworld(self):
         self.assertEqual(countCommits('Arceus1ooo', 'helloworld'), 4)
-    def test_Exchange4Students(self):
-        self.assertEqual(countCommits('Arceus1ooo', 'Exchange4Students'), 96)
+    def test_HW3(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW3'), 3)
     def test_HW5345(self):
         self.assertEqual(countCommits('Arceus1ooo', 'HW5-345'), 4)
-    def Test_DungeonKnight(self):
-        self.assertEqual(countCommits('Arceus1ooo', 'Dungeon-Knight'), 134)
-    def test_HW4345(self):
-        self.assertEqual(countCommits('Arceus1ooo', 'HW4-345'), 490)
+    def Test_DesignPatternLab(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'DesignPatternLab'), 2)
+    def test_Complexity(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW4-345'), 2)
 
 if __name__ == '__main__':
     print('Running unit tests')

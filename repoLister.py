3e5e4b927c2cc0f62c6dd8e58c24a682b338c8d3
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2021-10-02 23:18:14 -0400

    changed file structure because import is not working

--- repoLister.py
+++ repoLister.py
@@ -1,6 +1,28 @@
 import json
 import requests
 from pprint import pprint
+import unittest
+
+class TestCommitCounter(unittest.TestCase):
+
+    def test_HW2567(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW2-567'), 12)
+    def test_HW1567(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW1-567'), 6)
+    def test_helloworld(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'helloworld'), 4)
+    def test_HW3(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW3'), 3)
+    def test_HW5345(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'HW5-345'), 4)
+    def test_DesignPatternLab(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'DesignPatternLab'), 2)
+    def test_Complexity(self):
+        self.assertEqual(countCommits('Arceus1ooo', 'Complexity'), 2)
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
 
 def countCommits(user, repoName):
     commitRequest = requests.get(f'https://api.github.com/repos/{user}/{repoName}/commits').json()
